90c2475b59605682803104527f25813a5ecdf1f8
unknown <sahan.20220255@iit.ac.lkgit config --global user.name zahan001>
2024-10-30 18:44:58 +0530

    adding css styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import EmployeeList from './components/EmployeeList';
-import EmployeeForm from './components/EmployeeForm';
-import { fetchEmployees } from './api/api'; // Make sure to import fetchEmployees
+import { fetchEmployees } from './api/api'; 
+import './App.css';
 
 const App = () => {
-    const [showForm, setShowForm] = useState(true);
     const [employees, setEmployees] = useState([]);
-
-    const handleToggleForm = () => {
-        setShowForm(!showForm);
-    };
 
     const loadEmployees = async () => {
         const empData = await fetchEmployees();
@@ -21,18 +16,10 @@ const App = () => {
         loadEmployees();
     }, []);
 
-    const handleEmployeeAdded = () => {
-        loadEmployees(); // Refresh the employee list
-    };
-
     return (
         <Router>
             <div className="app-container">
                 <h1>Employee Management System</h1>
-                <button onClick={handleToggleForm}>
-                    {showForm ? 'Hide Form' : 'Add Employee'}
-                </button>
-                {showForm && <EmployeeForm onEmployeeAdded={handleEmployeeAdded} />}
                 <EmployeeList employees={employees} />
             </div>
         </Router>
